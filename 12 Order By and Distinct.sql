d8127d16b5cb2a05767b0aebcc92fbd6bfd19f4c
bcShuvam <bcshuvam@gmail.com>
2021-05-09 17:37:27 +0545

    D1 Order By and Distinct

---
+++ 12 Order By and Distinct.sql
@@ -0,0 +1,17 @@
+-- 'asc' for asscending order and 'desc' for descending order 
+
+-- select * from personal
+-- order by name desc;
+
+-- select * from personal
+-- order by id desc;
+
+-- select * from personal
+-- where city = "Biratnagar"
+-- order by name asc;
+
+-- select * from personal
+-- order by name,city;
+
+-- select distinct city from personal;
+select distinct age from personal order by age;
