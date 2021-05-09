e4c63c76acaf126ef5b2650d86adf94f5d43741e
bcShuvam <bcshuvam@gmail.com>
2021-05-09 12:30:23 +0545

    D1 Insert Multiple Row

---
+++ 4 Insert Multiple Rows.sql
@@ -0,0 +1,5 @@
+insert into student.personal(id,name,birth_date,phone,gender)
+values
+(3,"Anil Kapoor","1991-10-05","94752544881","M"),
+(4,"Juhi Chawla","1992-03-25","94752544881","F"),
+(5,"John Abraham","1992-02-07","94753344881","M");
