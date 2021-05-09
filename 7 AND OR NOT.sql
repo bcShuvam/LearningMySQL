bed85b05ad783015504bc91bfb23257b9d7f978f
bcShuvam <bcshuvam@gmail.com>
2021-05-09 14:11:33 +0545

    D1 AND OR NOT Operators

---
+++ 7 AND OR NOT.sql
@@ -0,0 +1,6 @@
+select * from personal
+-- where age >= 18 and age <= 20 and gender = "F" and city <> "Biratnagar";
+-- where city = "Biratnagar" OR city = "Kathmandu" and gender = "M";
+-- where (city = "Biratnagar" OR city = "Kathmandu") and gender = "M";
+where not age >= 20;
+-- where not (city = "Biratnagar" OR city = "Kathmandu");
