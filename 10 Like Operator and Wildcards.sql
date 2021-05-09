cb44040d29c2faf5e49b1f91221990421c03e4ef
bcShuvam <bcshuvam@gmail.com>
2021-05-09 16:45:22 +0545

    D1 Like Operators and Wildcards

---
+++ 10 Like Operator and Wildcards.sql
@@ -0,0 +1,9 @@
+select * from personal
+-- where name like "r%" or name like "s%";
+-- where name not like "r%";
+-- where BINARY name like "r%"; -- Use BINARY after where For case sensetive
+-- where name like "%ar"; 
+-- where phone like "%55"; 
+-- where name like "s%n"; 
+-- where name like "__m%"; 
+where name like "_a_%"; 
