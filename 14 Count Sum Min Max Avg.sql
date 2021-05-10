c3f7b2c56d25019231b264b152aadf202f0fc424
bcShuvam <bcshuvam@gmail.com>
2021-05-10 12:24:25 +0545

    D2 Count Sum Min Max Avg

---
+++ 14 Count Sum Min Max Avg.sql
@@ -0,0 +1,11 @@
+-- select count(name) from personal;
+
+-- select count(distinct city) as Total from personal;
+
+-- select max(percentage) as percentage from personal;
+
+-- select min(percentage) as percentage,name,city from personal;
+
+select sum(percentage) as total from personal;
+
+select avg(percentage) as total from personal;
