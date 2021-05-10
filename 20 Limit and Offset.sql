f066ad2f3c9de15c9d00bfeba3438a8d9802d66f
bcShuvam <bcshuvam@gmail.com>
2021-05-10 14:07:31 +0545

    D2 Limit and Offset

---
+++ 20 Limit and Offset.sql
@@ -0,0 +1,11 @@
+-- select * from personal
+-- limit 3;
+
+-- select * from personal
+-- where id between 2 and 10 -- Where clause is optional 
+-- order by name -- order by is also is optional 
+-- limit 3;
+
+-- select * from personal;
+-- limit 0,3;
+-- limit 5,3;
